edfe8dc10a71d0033ab946796e2079b3130d61eb
OhNiMiru <anastasiyaskrypova@gmail.com>
2026-04-27 18:10:21 +0300

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,44 @@
+// ========== НАСТРОЙКА ==========
+const CENTRAL_API_URL = "https://script.google.com/macros/s/AKfycby4lJ709y0PTT2LpYvUHcv9IYPsNOPrghn8J5svksHzR0lvjauAcNwy-YW7QevBJmR-/exec";
+
+let CURRENT_USER = {
+    id: null,
+    name: null,
+    role: null,
+    sheetUrl: null,
+    shareStats: false,
+    hideStats: false
+};
+
+let pendingOperations = [];
+let isOnline = navigator.onLine;
+
+// ========== ОСНОВНЫЕ ПЕРЕМЕННЫЕ ==========
+let originalCardsData = [];
+let currentFilteredData = [];
+let isLoading = false;
+let autoRefreshInterval = null;
+let historySyncInterval = null;
+let currentEditIndex = null;
+let selectedTypes = new Set();
+let currentSortBy = "none";
+let typeOptions = [];
+let customOrder = [];
+let dragStartIndex = null;
+let selectedDiscountProducts = new Set();
+let discountProductListVisible = false;
+let itemDiscounts = {};
+let discountPanelOpen = false;
+let typeColors = new Map();
+const colorPalette = ['#e67e22', '#f39c12', '#2ecc71', '#3498db', '#9b59b6', '#e74c3c', '#1abc9c', '#f1c40f', '#e67e22', '#95a5a6'];
+
+let cart = {};
+let extraCosts = [];
+let globalExtraCosts = [];
+let salesHistory = [];
+let historyMethodFilter = "all";
+let historyTypeFilter = "all";
+let rulesList = [];
+let currentRuleType = "type";
+let selectedProducts = new Set();
+let scrollPosition = 0;
