4dba7385d962f1f2fc9d05fbf179fb3d0500388c
OhNiMiru <anastasiyaskrypova@gmail.com>
2026-05-02 02:39:05 +0300

    Update [[path]].js

--- functions/api/[[path]].js
+++ functions/api/[[path]].js
@@ -79,7 +79,7 @@ export async function onRequest(context) {
     }
     
     // Прокси для Google Apps Script (все остальные запросы к /api)
-    const GS_API_URL = "https://script.google.com/macros/s/AKfycbz-dLz4PRcN32IZ_Jlg04buhcqygTjE9_WL8xtktnH7lEJQ2jWoF5Acf4K6PER9K_Gv/exec";
+    const GS_API_URL = "https://script.google.com/macros/s/AKfycbxiD2dRoBtogTdWtAMfkXeu_QgWb4xv5fA7nUatlXOHZwUSaKM9QlLAjRYPGmyXZ64R/exec";
     
     const gsUrl = new URL(GS_API_URL);
     for (const [key, value] of url.searchParams.entries()) {
